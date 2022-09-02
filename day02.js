4de5ec657251ebf05d9e514376c3f20b701e8dd5
hynlog2520 <74488059+hynlog2520@users.noreply.github.com>
2022-09-03 04:36:57 +0700

    first commit

---
+++ day02.js
@@ -0,0 +1,61 @@
+function bai1(){
+    console.log("bai1")
+    console.log('"I love you"');
+}
+bai1();
+
+function bai2(numberA,numberB){
+    console.log("\nbai2");
+    console.log(numberA/numberB);
+}
+bai2(8,2);
+
+function bai3(x,y){
+    console.log("\nbai3");
+    console.log(x+y);
+}
+bai3(1,6);
+
+function bai4(F){
+    console.log("\nbai4");
+    console.log() // viet code bi sai cho` thay` giang lai 
+}
+bai4();
+
+function bai5(a,b){
+    console.log("\nbai5");
+    console.log(a*a+b*b);
+}
+bai5(2,3);
+
+function bai6(a,b){
+    console.log("\nbai6");
+    let c = (a+b)**2;
+    console.log(c);
+}
+bai6(3,4);
+
+function bai7(a,b,c){
+    console.log("\nbai7");
+    let d=Math.pow(a*b,2);
+    console.log(d/c);
+}
+bai7(3,2,4);
+
+function bai8(a,b,c){
+    console.log("\nbai8");
+    let tbc=(a+b+c)/3;
+    console.log(tbc);
+}
+bai8(1,2,3)
+
+function bai9(a,b){
+    console.log("\nbai9");
+    let c=b/a;
+    console.log(typeof c);
+}
+bai9(2,0);
+
+
+
+
